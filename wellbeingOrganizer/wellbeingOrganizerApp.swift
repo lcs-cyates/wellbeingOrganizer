61366ef66f5e7281d5f6b0594c958a52fdf2f062
Clea Yates <cyates@lcs.on.ca>
2020-12-04 11:02:57 -0500

    Initial Commit

---
+++ wellbeingOrganizer/wellbeingOrganizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  wellbeingOrganizerApp.swift
+//  wellbeingOrganizer
+//
+//  Created by Yates, Clea on 2020-12-04.
+//
+
+import SwiftUI
+
+@main
+struct wellbeingOrganizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
